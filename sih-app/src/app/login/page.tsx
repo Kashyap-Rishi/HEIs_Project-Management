033f706a527fa7664183b335399e43a73f02e79a
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-09-23 15:10:04 +0530

    prisma plus login

---
+++ sih-app/src/app/login/page.tsx
@@ -0,0 +1,29 @@
+import styles from "./login.module.css"
+import "./login.css"
+const Login = () =>{
+    return(
+        <div className={styles.logingrandparent}>
+            <div className={styles.loginparent}>
+                <div className={styles.formdiv}>
+                    <form className="flex flex-col formelemet">
+                        <div className={styles.formheading}>Login</div>
+                        <div>
+                        <input placeholder="Username" className={styles.username}></input>
+                        </div>
+                       <div>
+                       <input placeholder="Password" className={styles.password}></input>
+                       </div>
+                       <div className={styles.loginbuttondiv}>
+                       <button className={styles.loginbutton}>Submit</button>
+
+                       </div>
+
+                    </form>
+                    <div className={styles.donthaveacc}>Dont have a account? <p className={styles.createacc}>create an account.</p></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
